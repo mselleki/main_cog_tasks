b58f16295262698c08d4920a3b2aa3bf8258179e
mselleki <mselleki98@gmail.com>
2022-05-05 10:26:11 +0200

    first version to test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # main_cog_tasks
+
+A program which allows to execute the four cognitive tasks we developed in a row. Outputs are saved in each task's csv/ folder.
---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+os.chdir('../seven_diff/')
+os.system('python seven_diff.py')
+
+os.chdir('../lucifer')
+os.system('python lucifer.py')
+
+os.chdir('../where_is_tockie')
+os.system('python where_is_tockie.py')
+
+os.chdir('../symetry')
+os.system('python symetry.py')
